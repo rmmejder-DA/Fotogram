46be3fb276a03b14159c7c4ecfd091607da574bb
MAX <rmmejder@gmail.com>
2025-12-15 21:57:46 +0100

    aufgeräumg/CSS/JS new

---
+++ main.js
@@ -0,0 +1,265 @@
+const dialogRef = document.getElementById('myDialog');
+const dialogImage = document.getElementById('dialogImage');
+
+let img = [
+  './image/katzen-4604843_1280.jpg',
+  './image/f07d322e-ef42-4e1e-90d5-a0a3284276a4 (2).JPG',
+  './image/d2058bfe-21c4-4015-9a00-6fa02f13d25a (2).JPG',
+  './image/d998f545-75d8-4822-bd76-41692cf74776 (2).JPG',
+  './image/cat-8612685_1280.jpg',
+  './image/bd374f12-c955-4e1b-9368-1f532babfa2c 1.JPG',
+  './image/03192c9a-4b0f-4372-9c87-6cec90261d2c (2).JPG',
+  './image/447afcc7-44a2-4916-87af-8297d2ac1c5b (2).JPG',
+  './image/66f1dc84-6c22-41db-9c4d-9a0d9407fbc6.JPG',
+  './image/6b8b4db4-7d47-4fb9-a581-ac9946f7a03c.JPG',
+  './image/5cd269ac-a0ee-439f-98c6-83978e743318 (2).JPG',
+  './image/5b6371e7-0edd-413b-961b-37198bb9be33 (2).JPG',
+  './image/2b561ab7-4458-4082-855f-98fdc5f66ebf (2).JPG',
+  './image/0e7d8016-9d3e-45f6-a34f-48d356fe27cc.JPG'
+];
+let img2 = [
+  './image/bear-8275920_1280.jpg',
+  './image/beetle-7997225_1280.jpg',
+  './image/berg.jpg',
+  './image/cows-7772777_1280.jpg',
+  './image/daisy-1406895_1280.jpg',
+  './image/fern-9872693_640.jpg',
+  './image/fly-637949_1280.jpg',
+  './image/mount-fuji-2297961_1280.jpg',
+  './image/nangchen-9918045_640.jpg',
+  './image/snake-9059936_1280.jpg',
+  './image/squirrel-8367079_1280.jpg',
+  './image/strawberry-7224875_1280.jpg',
+  './image/volcano-3779159_1280.jpg',
+  './image/road-1072821_1280.jpg'
+];
+let img3 = [
+  './image/car-49278_1280.jpg',
+  './image/car-race-438467_1280.jpg',
+  './image/car-63930_1280.jpg',
+  './image/sport-4155825_1920.jpg',
+  './image/automobile-2406393_1280.jpg',
+  './image/car-2275763_1280.jpg',
+  './image/ferrari-190036_1280.jpg',
+  './image/giugiaro-753852_1280.jpg',
+  './image/mclaren-4535102_1280.jpg',
+  './image/model-car-2091416_1280.jpg',
+  './image/nissan-silvia-s15-4418190_1280.jpg',
+  './image/race-car-1112032_1280.jpg',
+  './image/sports-car-6282703_1280.jpg',
+  './image/supercar-8589586_640.png'
+];
+let img4 = [
+  './image/christmas-7668272_1280.jpg',
+  './image/gingerbread-7666269_1280.jpg',
+  './image/star-6803666_1280.jpg',
+  './image/table-setting-6859274_1280.jpg',
+  './image/advent-1104854_1280.jpg',
+  './image/bullet-65825_1280.jpg',
+  './image/characters-1690782_1280.jpg',
+  './image/christmas-5850470_1280.jpg',
+  './image/christmas-balls-6862056_1280.jpg',
+  './image/christmas-market-7664171_1280.jpg',
+  './image/nutcracker-3023998_1280.jpg',
+  './image/nuts-3853208_1280.jpg',
+  './image/sheet-music-8398449_1280.jpg',
+  './image/star-514848_1280.jpg'
+];
+let img5 = [
+  './image/birds-217590_1280.jpg',
+  './image/eagle-1450672_1280.jpg',
+  './image/eagle-1753002_1280.jpg',
+  './image/head-723540_1280.jpg',
+  './image/adler-4840580_1280.png',
+  './image/adler-5544260_1280.jpg',
+  './image/animal-3938221_1280.jpg',
+  './image/attack-3630365_1280.jpg',
+  './image/bald-eagles-3112610_1280.jpg',
+  './image/beer-3786988_1280.jpg',
+  './image/bird-6342958_1280.jpg',
+  './image/eagle-5822300_1280.jpg',
+  './image/eagle-5822303_1280.jpg',
+  './image/eagle-6239046_1280.jpg',
+  './image/eagle-8849052_1280.jpg'
+];
+let img6 = [
+  './image/squirrel-barbary-3492406_1280.jpg',
+  './image/pipit-5683656_1280.jpg',
+  './image/monk-parakeet-2884519_1280.jpg',
+  './image/lighthouse-8578322_1280.jpg',
+  './image/beach-2793276_1280.jpg',
+  './image/birds-3476418_1280.jpg',
+  './image/coast-5870088_1280.jpg',
+  './image/costa-3910743_1280.jpg',
+  './image/crab-1022645_1280.jpg',
+  './image/fuerteventura-4859204_1280.jpg',
+  './image/landscape-4884972_1280.jpg',
+  './image/ruddy-turnstone-5800759_1280.jpg',
+  './image/surf-900287_1280.jpg',
+  './image/volcano-2352450_1280.jpg'
+];
+let img7 = [
+  './image/shepherd-dog-4357790_1280.jpg',
+  './image/dogs-8613175_1280.jpg',
+  './image/dogs-6463032_1280.jpg',
+  './image/dogs-4960444_1280.jpg',
+  './image/ai-generated-9437073_1280.png',
+  './image/cute-1917049_1280.jpg',
+  './image/dogs-1299485_1280.png',
+  './image/dogs-2207923_1280.jpg',
+  './image/dogs-7248400_1280.jpg',
+  './image/great-dane-3011826_1280.jpg',
+  './image/kennels-for-pets-3821861_1280.jpg',
+  './image/labradors-7855208_1280.jpg',
+  './image/pets-962215_1280.jpg',
+  './image/puppy-1040951_1280.jpg'
+];
+let img8 = [
+  './image/browser-773215_1280.png',
+  './image/earth-2254769_1280.jpg',
+  './image/tablet-5623396_1280.jpg',
+  './image/technology-6701504_1280.jpg',
+  './image/apple-1867762_1280.jpg',
+  './image/big-895567_1280.jpg',
+  './image/computer-5623398_1280.jpg',
+  './image/eye-669157_1280.jpg',
+  './image/future-7130603_1280.jpg',
+  './image/industry-4330186_1280.jpg',
+  './image/internet-1020137_1280.jpg',
+  './image/job-7291427_1280.png',
+  './image/monkey-4042658_1280.jpg',
+  './image/web-1045994_1280.jpg'
+];
+let img9 = [
+  './image/doll-figures-3015495_1280.jpg',
+  './image/santa-claus-1614994_1280.png',
+  './image/stollen-6848167_1280.jpg',
+  './image/xmas-2703168_1280.jpg',
+  './image/christmas-210289_1280.jpg',
+  './image/christmas-background-1869989_1280.jpg',
+  './image/christmas-background-4669449_1280.jpg',
+  './image/christmas-tree-574742_1280.jpg',
+  './image/christmas-tree-2928142_1280.jpg',
+  './image/christmas-tree-5819465_1280.jpg',
+  './image/merry-christmas-6691616_1280.png',
+  './image/moscow-3872942_1280.jpg',
+  './image/snowflake-1918794_1280.jpg',
+  './image/snowman-8413769_1280.jpg'
+];
+let img10 = [
+  './image/code-1839406_1280.jpg',
+  './image/digitization-6892337_1280.jpg',
+  './image/hacker-5027679_1280.jpg',
+  './image/javascript-736401_1280.png',
+  './image/ai-generated-8329581_1280.jpg',
+  './image/code-geek-2680204_640.png',
+  './image/developer-8829709_1280.jpg',
+  './image/emoji-4826138_640.png',
+  './image/factory-7281686_1280.png',
+  './image/hello-world-1333103_1280.jpg',
+  './image/mockup-5290462_1280.jpg',
+  './image/programmer-7633812_1280.jpg',
+  './image/technician-3652287_1280.jpg',
+  './image/web-3157323_1280.jpg'
+];
+let img11 = [
+  './image/rainbow-4047523_1280.jpg',
+  './image/rainbow-5324147_1280.jpg',
+  './image/rainbow-7350780_1280.jpg',
+  './image/waterfall-4751586_1280.jpg',
+  './image/ai-generated-7819238_1280.jpg',
+  './image/ai-generated-8316188_1280.jpg',
+  './image/bubble-3026504_1280.jpg',
+  './image/eye-3348431_1280.jpg',
+  './image/rainbow-26389_1280.png',
+  './image/rainbow-1513852_1280.jpg',
+  './image/rainbow-2362803_1280.jpg',
+  './image/rainbow-5156768_1280.jpg',
+  './image/rainbow-9440893_1280.jpg',
+  './image/soap-bubble-3490954_1280.jpg'
+];
+let img12 = [
+  './image/matterhorn-3019429_1280.jpg',
+  './image/snow-7658399_1280.jpg',
+  './image/village-9190397_1280.jpg',
+  './image/winter-9748652_1280.jpg',
+  './image/bird-8424336_1280.jpg',
+  './image/chiemsee-lake-8523044_1280.jpg',
+  './image/field-8419729_1280.jpg',
+  './image/houses-9116899_1280.png',
+  './image/icicle-7631570_1280.jpg',
+  './image/pine-cone-8454689_1280.jpg',
+  './image/robin-8373155_1280.jpg',
+  './image/soap-bubble-1986657_1280.jpg',
+  './image/train-6907884_1280.jpg',
+  './image/truck-4666300_1280.jpg'
+];
+
+// document.images
+let currentImage = 0;
+let sliderImages = img;
+
+function openDialog() {
+  if (dialogRef) dialogRef.showModal();
+  const altInfoRef = document.getElementById('alt-info');
+  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
+}
+function closeDialog() {
+  if (dialogRef) dialogRef.close();
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeDialog();
+  }
+  if (event.key === 'ArrowRight') {
+    document.getElementById("nextButton").click();
+  }
+  if (event.key === 'ArrowLeft') {
+    document.getElementById("prevButton").click();
+  }
+});
+
+function renderFiltered(index) {
+  const imageArrays = [img, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12];
+
+  if (index >= 1 && index <= imageArrays.length) {
+    render(imageArrays[index - 1]);
+  }
+}
+
+function render(arrimg) {
+  let contentRef = document.getElementsByClassName('dialogImage')[0];
+  contentRef.innerHTML = '';
+  sliderImages = arrimg;
+  currentImage = 0;
+  for (let i = 0; i < arrimg.length; i++) {
+    if (i === 0) {
+      dialogImage.src = sliderImages[currentImage];
+    }
+    contentRef.innerHTML += getNotesHtml(i, arrimg);
+  }
+}
+function getNotesHtml(i, arr) {
+  if (onload === true) {
+    currentImage = 0;
+    dialogImage.src = sliderImages[currentImage];
+  }
+  return `<div class='dialogContent'><img src="${arr[i]}" alt="[$sliderImage.alt]" style="cursor:pointer;" onclick="currentImage=${i}; dialogImage.src=sliderImages[${i}]; openDialog();"></div>`;
+}
+
+document.getElementById("prevButton").onclick = function () {
+  if (!sliderImages || sliderImages.length === 0) return;
+  currentImage = (currentImage - 1 + sliderImages.length) % sliderImages.length;
+  dialogImage.src = sliderImages[currentImage];
+  const altInfoRef = document.getElementById('alt-info');
+  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
+};
+
+document.getElementById("nextButton").onclick = function () {
+  if (!sliderImages || sliderImages.length === 0) return;
+  currentImage = (currentImage + 1) % sliderImages.length;
+  dialogImage.src = sliderImages[currentImage];
+  const altInfoRef = document.getElementById('alt-info');
+  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
+};
