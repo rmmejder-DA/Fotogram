05f5c8e76d41246fc1a82d3719487a33254ca469
MAX <rmmejder@gmail.com>
2025-12-28 16:48:26 +0100

    script ohne erfolg

--- script.js
+++ script.js
@@ -41,6 +41,14 @@ let dialogImage = document.getElementById('dialogImage');
 
 let AltTextRef = document.getElementById('imageInfo');
 
+function render(currentSiteimages = siteimages) {
+  let SiteimgRef = document.getElementById('contentimg');
+  SiteimgRef.innerHTML = '';
+  for (let i = 0; i < currentSiteimages.length; i++) {
+    SiteimgRef.innerHTML += getNotesHtml(i, currentSiteimages);
+  }
+}
+
 function renderFiltered(index) {
   currentImage = 0;
   if (index >= 1 && index <= AllimageArray.length) {
@@ -52,38 +60,22 @@ function renderFiltered(index) {
   updateImageInfo(sliderImages[0]);
 }
 
-function updateImageInfo() {
-  if (!sliderImages || sliderImages.length === 0) return;
-  else openDialog();
-  AltTextRef.textContent = `${imageCategories[AllimageArray.indexOf(sliderImages)]}`;
-  dialogImage.src = sliderImages[currentImage];
-  const altInfoRef = document.getElementById('alt-info');
-  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
-};
-
-function render(currentSiteimages = siteimages) {
-  let SiteimgRef = document.getElementById('contentimg');
-  SiteimgRef.innerHTML = '';
-  for (let i = 0; i < currentSiteimages.length; i++) {
-    SiteimgRef.innerHTML += getNotesHtml(i, currentSiteimages);
-  }
-}
-
 function getNotesHtml(i, siteimages) {
   return `<div class="image_grid_item">
               <p class="image_grid_item_p">${imageCategories[i]}</p>
-              <img class="image_grid_img" onclick="openDialog();renderFiltered(${i + 1});"src="${siteimages[i]}" alt="${imageCategories[currentImage]}"/>
+              <button style="background-color: transparent;border-radius: 25px; border:none; padding:0;" class="filter_button" onclick="renderFiltered(${i + 1});">
+              <img class="image_grid_img" onclick="openDialog()"src="${siteimages[i]}" alt="${imageCategories[currentImage]}"/>
+              </button>
         </div>`;
 };
-document.addEventListener('keydown', function (event) {
-  if (dialogRef.open) {
-    if (event.key === 'ArrowLeft') {
-      prev();
-    } else if (event.key === 'ArrowRight') {
-      next();
-    }
-  }
-});
+
+function openDialog(i) {
+  currentImage = i || 0;
+  dialogImage.src = sliderImages[currentImage];
+  if (dialogRef) dialogRef.showModal();
+  const altInfoRef = document.getElementById('alt-info');
+  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
+}
 
 function prev() {
   if (!sliderImages || sliderImages.length === 0) return;
@@ -97,15 +89,23 @@ function next() {
   updateImageInfo();
 }
 
-function openDialog(i) {
-  currentImage = i || 0;
-  dialogImage.src = sliderImages[currentImage];
-  if (dialogRef) dialogRef.showModal();
-  const altInfoRef = document.getElementById('alt-info');
-  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
-}
-
+document.addEventListener('keydown', function (event) {
+  if (dialogRef.open) {
+    if (event.key === 'ArrowLeft') {
+      prev();
+    } else if (event.key === 'ArrowRight') {
+      next();
+    }
+  }
+});
 function closeDialog() {
   if (dialogRef) dialogRef.close();
+};
+
+function updateImageInfo() {
+  AltTextRef.textContent = `${imageCategories[AllimageArray.indexOf(sliderImages)]}`;
+  dialogImage.src = sliderImages[currentImage];
+  const altInfoRef = document.getElementById('alt-info');
+  altInfoRef.textContent = `${currentImage + 1} / ${sliderImages.length}`;
 };
 render();
